6466841ab7278dc72e3a70297b51d12a9bfb569f
chck <iwazaki_yuki@cyberagent.co.jp>
2016-08-01 02:55:13 +0900

    add chart.js

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,20 +20,74 @@
             span.glyphicon.glyphicon-info-sign.alert-icon-float-left
             strong Heads up!
             p This alert needs your attention.
+    .row
+      .col-md-6
+        chart(type="radar", :data="chartData", :options="chartOptions")
+      .col-md-6
+        chart(type="line", :data="chartData", :options="chartOptions")
+    .row
+      .col-md-6
+        chart(type="bar", :data="chartData", :options="chartOptions")
+      .col-md-6
+        chart(type="polarArea", :data="chartData", :options="chartOptions")
+    .row
+      .col-md-6
+        chart(type="pie", :data="chartData", :options="chartOptions")
+      .col-md-6
+        chart(type="doughnut", :data="chartData", :options="chartOptions")
 </template>
 
 <script>
   import { alert } from 'vue-strap'
+  import chart from './Chart.vue'
 
   export default {
     components: {
-      alert
+      alert, chart
     },
     data() {
       return {
         title: 'Sibyl',
         showRight: false,
-        showTop: false
+        showTop: false,
+        user: {
+          id: 'xxx',
+          name: 'chck'
+        },
+        chartType: 'bar',
+        chartData: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        chartOptions: {
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero:true
+              }
+            }]
+          }
+        }
       }
     }
   }
---
+++ frontend/src/Chart.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  canvas(height="height", width="width")
+</template>
+
+<script>
+  import Chart from 'chart.js'
+
+  export default {
+    props: {
+      height: String,
+      width: String,
+      type: {type: String, default: 'line'},
+      data: Object,
+      options: Object
+    },
+    ready() {
+      setTimeout(() => {
+        new Chart(this.$el, {
+          type: this.type,
+          data: this.data,
+          options: this.options
+        })
+      }, 100)
+    }
+  }
+</script>
